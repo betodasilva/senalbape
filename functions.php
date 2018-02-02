614b0183af6be8936466f099e1682d239dc8e136
betodasilva <jrs_robert@hotmail.com>
2018-02-02 13:46:23 -0300

    Style and adjust widgets

--- functions.php
+++ functions.php
@@ -111,8 +111,8 @@ function senalbape_widgets_init() {
 		'description'   => esc_html__( 'Add widgets here.', 'senalbape' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'senalbape_widgets_init' );
