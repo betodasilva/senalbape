969c9bef5dfe1be6661d009bde561139cbf14d53
betodasilva <jrs_robert@hotmail.com>
2018-02-19 15:35:42 -0300

    structure archive layout

--- archive.php
+++ archive.php
@@ -10,38 +10,35 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main container">
+			<div class="row gutters">
+				<div class="col col-8">
+						<?php if ( have_posts() ) : ?>
+							<header class="page-header">
+								<h1 class="page-title">&raquo; <?php single_term_title(); ?></h1>
+							</header><!-- .page-header -->
 
-		<?php
-		if ( have_posts() ) : ?>
+							<?php
+							while ( have_posts() ) : the_post();
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+								get_template_part( 'template-parts/content', 'fp' );
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+							endwhile; // End of the loop.
+							
+							the_posts_navigation();
+							
+						else :
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+							get_template_part( 'template-parts/content', 'none' );
 
-			endwhile;
+						endif; ?>
+				</div>
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+				<div class="col col-4">
+					<?php get_sidebar(); ?>
+				</div>
+	
+				<hr>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
